16f9acb7f89b58cb17a1b1c053173c1c0b12cc72
christianmarrtinez <christianmarrtinez@gmail.com>
2024-12-15 02:09:42 -0500

    begin entry page specification

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
+import markdown2
 
 
 def index(request):
@@ -8,3 +9,18 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    content = util.get_entry(title) ## Retrieve Markdown content ##
+    if content is None:
+        # if the entry doesn't exist, render an error page
+        return render(request, "encyclopedia.error.html", {
+            "message": f"'{title}' entry not found."
+        })
+    else:
+        # convert Markdown to HTML
+        html_content = markdown2.markdown(content)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+        })
